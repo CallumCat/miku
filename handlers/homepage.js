cc21e2d2b1a248509c70382f8e0cc8aa65461297
Alicia <gw17maleckimaksym@glow.sch.uk>
2018-09-12 22:32:11 +0100

    lets look epic

--- handlers/homepage.js
+++ handlers/homepage.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
+const { query } = require("../objects/db");
+
+async function usersStuff(i) {
+    const q = await query("SELECT * FROM users WHERE id = ?", i)
+    return q
+}
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        url: req.headers.host
-    });
+    usersStuff(req.user).then(q => {
+        if (req.user) {
+            res.render("index", {
+                url: req.headers.host,
+                loggedIn: req.isAuthenticated(),
+                username: q[0].username
+            });
+        } else {
+            res.render("index", {
+                url: req.headers.host,
+                loggedIn: req.isAuthenticated(),
+                username: ""
+            });
+        }
+    })
 })
 
 module.exports = app;
